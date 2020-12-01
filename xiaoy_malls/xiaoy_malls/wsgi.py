c4ded0dd731084acb74cbdce9538fcdcfab99d57
alone <alone@MacBook-Pro.local>
2020-12-01 17:32:28 +0800

    提交git管理日志的配置文件

---
+++ xiaoy_malls/xiaoy_malls/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for xiaoy_malls project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xiaoy_malls.settings")
+
+application = get_wsgi_application()
